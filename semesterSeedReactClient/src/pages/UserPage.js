22b98f441d9a3977e19cc2c48dcf47549e7aae06
DjKirkovic <kirkovic@gmail.com>
2017-06-09 14:46:10 +0200

    Update

--- semesterSeedReactClient/src/pages/UserPage.js
+++ semesterSeedReactClient/src/pages/UserPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from "mobx-react";
-import userData from "../stores/userStore";
+import userStore from "../stores/userStore";
 
 const UserPage = observer(
     class UserPage extends Component {
@@ -10,18 +10,30 @@ const UserPage = observer(
              This will fetch data each time you navigate to this route
              Move to constructor, if only required once, or add "logic" to determine when data should be "refetched"
              */
-            userData.getData();
+            userStore.getData();
         }
 
         render() {
+            var lis = userStore.clubs.map((club)=>{
+                    return(
+                        <li>{club.name} <a href={club.url}/></li>
+                        // <li>{club.name}, {club.url}></li>
+                    )
+                }
+                // }
+
+            )
             return (
                 <div>
                     <h2>Users</h2>
                     <p>This message is fetched from the server if you are properly logged in</p>
                     <div className="msgFromServer">
-                        {userData.messageFromServer}
+                        {userStore.messageFromServer}
+                        <ul>
+                            {lis}
+                        </ul>
                     </div>
-                    <h4 style={{ color: "red" }}>{userData.errorMessage}</h4>
+                    <h4 style={{ color: "red" }}>{userStore.errorMessage}</h4>
                 </div>
             )
         }
